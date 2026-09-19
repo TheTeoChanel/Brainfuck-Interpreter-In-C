8f490fc91c1d88c0d9eae2b754b241893de201c6
TheTeoChanel <133592784+TheTeoChanel@users.noreply.github.com>
2026-09-19 14:23:29 +0300

    Update bf.c

--- bf.c
+++ bf.c
@@ -9,19 +9,16 @@ int main(int argc, char *argv[]) {
 
 	if (argc == 1) {
 		printf("fatal error: no input file\n");
-		printf("use -h to get help\n");
+		printf("use --help to get help\n");
 		return 1;
 	}
-
-	if (argc > 2 && strcmp(argv[2], "-d")==0)
-		debugMode = true;
-
-	if (strcmp(argv[1], "-v")==0) {
-		printf("BrainF*ck Interpreter by TheTeoChannel\nVersion 0.11 // 2026\n");
+	// VERSION
+	if (strcmp(argv[1], "--version")==0) {
+		printf("BrainF*ck Interpreter by TheTeoChannel\nVersion 0.12 // 2026\n");
 		return 0;
 	}
-
-	if (strcmp(argv[1], "-h")==0) {
+	// HELP
+	if (strcmp(argv[1], "--help")==0) {
 		printf("BRAINF*CK Interpreter by TheTeoChanel\n");
 		printf("\n");
 		printf("Syntax:\n\n");
@@ -30,12 +27,25 @@ int main(int argc, char *argv[]) {
 		printf("bf -OPTION\n");
 		printf("\n");
 		printf("Options:\n\n");
-		printf("-h		open this page (not a self reference)\n");
-		printf("-t		TRACE MODE: EVERY ACTION SAVED IN LOGS.TXT\n");
-		printf("-d		DEBUG MODE: Printing important actions ('[', ']', '.', ',')\n");
-		printf("-v		Show version\n");
+		printf("--help		open this page (not a self reference)\n");
+		printf("-t			TRACE MODE: Tracking every command in logs.txt\n");
+		printf("-d			DEBUG MODE: Printing only important actions ([ ] . ,)\n");
+		printf("--version	Show version of this program\n");
 		printf("\nHad fun making this, TheTeoChanel 2026\n");
 		return 0;
+	}
+	if (argc > 2 && strcmp(argv[2], "-d")==0)
+		debugMode = true;
+	else if (argc > 2) {
+		printf("fatal error: unrecognized option '%s'.", argv[2]);
+		return 1;
+	}
+
+	if (argc > 2 && strcmp(argv[2], "-t")==0)
+		traceMode = true;
+	else if (argc > 2) {
+		printf("fatal error: unrecognized option '%s'.", argv[2]);
+		return 1;
 	}
 
 	FILE *logs;
@@ -45,26 +55,24 @@ int main(int argc, char *argv[]) {
 	fprintf(logs, "!PRINT!, cell:CELL NUM, val:CELL VALUE, char:CELL VALUE CHAR\n");
 	fprintf(logs, "-------------------------------------------------\n");
 
-	if (argc > 2 && strcmp(argv[2], "-t")==0)
-		traceMode = true;
-
 	int stack[30000] = {0};
 	int ptr = 0;
 	int depth = 1;
 	char program[1024];
 	char buf[2];
 
+	//INIT FILE
 	FILE *fptr;
 	fptr = fopen(argv[1], "r");
 
 	if (fptr == NULL) {
-		printf("fatal error: couldnt open input file\n");
+		printf("fatal error: couldnt open the input file\n");
 		return 1;
 	}
 
+	//READ BF FILE
 	int programLen = fread(program, 1, sizeof(program) - 1, fptr);
 	program[programLen] = '\0';
-
 	fclose(fptr);
 
 	for (int i = 0; i < programLen; i++) {
@@ -102,7 +110,6 @@ int main(int argc, char *argv[]) {
 		case ',':
 			printf("USER INPUT> ");
 			fgets(buf, sizeof(buf), stdin);
-			printf("\n");
 			stack[ptr] = (int)buf[0];
 			break;
 
@@ -133,7 +140,6 @@ int main(int argc, char *argv[]) {
 			break;
 		}
 	}
-
 	fclose(logs);
 	return 0;
 }
